7d094881c3781396a38c749293ce8da993eb1854
Anil <khalifaanil84@gmail.com>
2025-04-27 13:16:21 +0545

    update payment controller and route with proper documentation

--- src/domains/payment/controller/index.js
+++ src/domains/payment/controller/index.js
@@ -1,10 +1,27 @@
+/**
+ * @file Payment/index.js
+ * @description This file contains the controller functions for the payment module.
+ * It handles the creation of payments, fetching payments by provider, requesting payments, and updating Khalti numbers.
+ * @module Payment
+ * @requires express
+ * @requires cloudinary
+ * @requirres multer
+ */
 import Payment from "../../../../models/Payment.js";
 import { getDataUris } from "../../../utils/Features.js";
 import catchAsync from "../../../utils/catchAsync.js";
 import cloudinary from "cloudinary";
 
-//create payment
-export const createPayment = catchAsync(async (req, res, next) => {
+/**
+ * @function createPayment
+ * @description Creates a new payment.
+ * @param req - The request object containing payment details.
+ * @param res - The response object to send the result.
+ * @returns - A JSON response with the created payment details or an error message.
+ * @throws - If an error occurs during payment creation, a 500 status code and error message are returned.
+ * @async
+ */
+export const createPayment = catchAsync(async (req, res) => {
   try {
     const { PaymentBy, PaymentTo, job, amount, payment_type } = req.body;
     const payment = await Payment.create({
@@ -21,7 +38,16 @@ export const createPayment = catchAsync(async (req, res, next) => {
   }
 });
 
-export const getPaymentByProvider = catchAsync(async (req, res, next) => {
+/**
+ * @function getPaymentByProvider
+ * @description Fetches payments made to a specific provider.
+ * @param req - The request object containing user details.
+ * @param res - The response object to send the result.
+ * @returns - A JSON response with the payment details or an error message.
+ * @throws - If an error occurs during fetching payments, a 500 status code and error message are returned.
+ * @async
+ */
+export const getPaymentByProvider = catchAsync(async (req, res) => {
   try {
     const payments = await Payment.find({
       PaymentTo: req.user._id,
@@ -44,8 +70,16 @@ export const getPaymentByProvider = catchAsync(async (req, res, next) => {
   }
 });
 
-//request payment update payment_status to request_payment
-export const requestPayment = catchAsync(async (req, res, next) => {
+/**
+ * @function requestPayment
+ * @description Requests payment by uploading confirmation images.
+ * @param req - The request object containing payment ID and image files.
+ * @param res - The response object to send the result.
+ * @returns - A JSON response with the updated payment details or an error message.
+ * @throws - If an error occurs during payment request, a 500 status code and error message are returned.
+ * @async
+ */
+export const requestPayment = catchAsync(async (req, res) => {
   try {
     const files = getDataUris(req.files);
 
@@ -71,8 +105,17 @@ export const requestPayment = catchAsync(async (req, res, next) => {
   }
 });
 
-//update khaltinumber
-export const updateKhaltiNumber = catchAsync(async (req, res, next) => {
+/**
+ * @function updateKhaltiNumber
+ * @description Updates the Khalti number for a payment.
+ * @param req - The request object containing payment ID and Khalti number.
+ * @param res - The response object to send the result.
+ * @returns - A JSON response with the updated payment details or an error message.
+ * @throws - If an error occurs during Khalti number update, a 500 status code and error message are returned.
+ * @async
+ *
+ */
+export const updateKhaltiNumber = catchAsync(async (req, res) => {
   try {
     const { recieverNumber } = req.body;
     const payment = await Payment.findById(req.params.id);
--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -1,3 +1,7 @@
+/**
+ * @file Payment.js
+ * @description This file contains the routes for the payment module in the application.
+ */
 import express from "express";
 import { protect, permission } from "../domains/auth/middlewares/auth.js";
 import {
@@ -9,17 +13,45 @@ import {
 import { multipleUpload } from "../domains/auth/middlewares/Multer.js";
 const router = express.Router();
 
+/**
+ * @description This route is used to create a payment.
+ * @route POST /api/v1/payment/createPayment
+ * @access Private
+ * @permission job_provider
+ */
 router
   .route("/createPayment")
   .post(protect, permission(["job_provider"]), createPayment);
+
+/**
+ * @description This route is used to get payment by provider.
+ * @route GET /api/v1/payment/getPaymentByProvider
+ * @access Private
+ * @permission job_seeker
+ */
 
 router
   .route("/getPaymentByProvider")
   .get(protect, permission(["job_seeker"]), getPaymentByProvider);
 
+/**
+ * @description This route is used to request payment.
+ * @route PUT /api/v1/payment/requestPayment/:id
+ * @access Private
+ * @param {string} id - The ID of the payment to request.
+ * @permission job_seeker
+ */
 router
   .route("/requestPayment/:id")
   .put(protect, permission(["job_seeker"]), multipleUpload, requestPayment);
+
+/**
+ * @description This route is used to update Khalti number.
+ * @route PUT /api/v1/payment/updateKhaltiNumber/:id
+ * @access Private
+ * @permission job_seeker
+ * @param {string} id - The ID of the payment to update.
+ */
 
 router
   .route("/updateKhalitNumber/:id")
